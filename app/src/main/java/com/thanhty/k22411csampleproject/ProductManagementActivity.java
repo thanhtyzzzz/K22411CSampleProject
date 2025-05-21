ae0a6d07dfb859930391bc2c692c9d9e7e38f252
thanhtyzzzz <thanhtylenguyen@gmail.com>
2025-05-21 21:19:53 +0700

    Commit

--- app/src/main/java/com/thanhty/k22411csampleproject/ProductManagementActivity.java
+++ app/src/main/java/com/thanhty/k22411csampleproject/ProductManagementActivity.java
@@ -1,46 +1,128 @@
 package com.thanhty.k22411csampleproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.thanhty.models.ListProduct;
+import com.thanhty.models.Category;
+import com.thanhty.models.ListCategory;
 import com.thanhty.models.Product;
 
 public class ProductManagementActivity extends AppCompatActivity {
 
-    ListView listViewProducts;
-    ListProduct listProduct;
+    Spinner spinnerCategory;
+    ArrayAdapter<Category> adapterCategory;
+    ListCategory listCategory;
+    ListView lvProduct;
+    ArrayAdapter<Product> adapterProduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_product_management);
-        addViews();
-        loadProducts();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        addViews();
+        addEvents();
+    }
+
+    private void addEvents() {
+        spinnerCategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Category c = adapterCategory.getItem(position);
+                displayProductByCategory(c);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Không làm gì khi không chọn danh mục
+            }
+        });
+
+        lvProduct.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Product p = adapterProduct.getItem(position);
+                displayProductDetail(p);
+            }
+        });
+    }
+
+    private void displayProductDetail(Product p) {
+        Intent intent = new Intent(ProductManagementActivity.this, ProductDetailActivity.class);
+        intent.putExtra("SELECTED_PRODUCT", p);
+        startActivity(intent);
+    }
+
+    private void displayProductByCategory(Category c) {
+        adapterProduct.clear();
+        adapterProduct.addAll(c.getProducts());
+        adapterProduct.notifyDataSetChanged();
     }
 
     private void addViews() {
-        listViewProducts = findViewById(R.id.listViewProducts);
+        spinnerCategory = findViewById(R.id.spinnerCategory);
+        adapterCategory = new ArrayAdapter<>(
+                ProductManagementActivity.this,
+                android.R.layout.simple_spinner_item);
+        adapterCategory.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCategory.setAdapter(adapterCategory);
+        listCategory = new ListCategory();
+        listCategory.generate_sample_product_dataset();
+        adapterCategory.addAll(listCategory.getCategories());
+
+        lvProduct = findViewById(R.id.lvProduct);
+        adapterProduct = new ArrayAdapter<>(ProductManagementActivity.this,
+                android.R.layout.simple_list_item_1);
+        lvProduct.setAdapter(adapterProduct);
     }
 
-    private void loadProducts() {
-        listProduct = new ListProduct();
-        listProduct.gen_dataset();
-        ArrayAdapter<Product> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, listProduct.getProducts());
-        listViewProducts.setAdapter(adapter);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu_product, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_new_product) {
+            Toast.makeText(ProductManagementActivity.this,
+                    "Mở màn hình thêm mới sản phẩm", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(ProductManagementActivity.this, ProductDetailActivity.class);
+            startActivity(intent);
+        } else if (item.getItemId() == R.id.menu_broadcast_advertising) {
+            Toast.makeText(ProductManagementActivity.this,
+                    "Gửi quảng cáo hàng loạt về sản phẩm", Toast.LENGTH_LONG).show();
+            // TODO: Tìm hiểu Firebase Cloud Messaging để gửi thông báo đẩy
+        } else if (item.getItemId() == R.id.menu_help) {
+            Toast.makeText(ProductManagementActivity.this,
+                    "Mở website hướng dẫn sử dụng", Toast.LENGTH_LONG).show();
+            // TODO: Mở trình duyệt với URL hướng dẫn
+            // Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://your-help-website.com"));
+            // startActivity(browserIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/thanhty/models/Product.java
+++ app/src/main/java/com/thanhty/models/Product.java
@@ -1,17 +1,45 @@
 package com.thanhty.models;
 
-public class Product {
+import androidx.annotation.NonNull;
+
+import java.io.Serializable;
+
+public class Product implements Serializable {
     private int id;
     private String name;
+    private int quantity;
     private double price;
+    private int cate_id;
+    private String description;
+    private int image_id;
 
-    public Product() {
-    }
-
-    public Product(int id, String name, double price) {
+    public Product(int id, String name, int quantity, double price, int cate_id, String description, int image_id) {
         this.id = id;
         this.name = name;
+        this.quantity = quantity;
         this.price = price;
+        this.cate_id = cate_id;
+        this.description = description;
+        this.image_id = image_id;
+    }
+
+    public Product(int id, String name, int quantity, double price, String description, int image_id) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+        this.description = description;
+        this.image_id = image_id;
+    }
+    public Product(int id, String name, int quantity, double price,  int image_id) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+        this.image_id = image_id;
+    }
+
+    public Product() {
     }
 
     public int getId() {
@@ -30,6 +58,14 @@ public class Product {
         this.name = name;
     }
 
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
     public double getPrice() {
         return price;
     }
@@ -38,8 +74,35 @@ public class Product {
         this.price = price;
     }
 
+    public int getCate_id() {
+        return cate_id;
+    }
+
+    public void setCate_id(int cate_id) {
+        this.cate_id = cate_id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public int getImage_id() {
+        return image_id;
+    }
+
+    public void setImage_id(int image_id) {
+        this.image_id = image_id;
+    }
+    @NonNull
     @Override
     public String toString() {
-        return name; // Hiển thị tên sản phẩm
+//        String info = id + "-" + name + "\nQuantity: " + quantity + "\nPrice: " + price + "\nCategory ID: " + cate_id + "\nDescription: " + description;
+        return id+"\t"+name+"\t"+price;
     }
 }
+
